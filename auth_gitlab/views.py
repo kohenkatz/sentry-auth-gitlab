bf8b180341cbc600b299d8dadf1f99fc42ad5a9a
MK <kohenkatz@gmail.com>
2021-11-29 10:05:46 -0500

    Fix copy-paste mistake

--- auth_gitlab/views.py
+++ auth_gitlab/views.py
@@ -7,7 +7,7 @@ from .client import GitLabClient
 
 class FetchUser(AuthView):
     def handle(self, request, helper):
-        with GitHubClient(helper.fetch_state("data")["access_token"]) as client:
+        with GitLabClient(helper.fetch_state("data")["access_token"]) as client:
             user = client.get_user()
             helper.bind_state('user', user)
             return helper.next_step()
